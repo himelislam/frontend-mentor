7c049cfcc2fd3b7bc71290f3d1955e44cac3a37a
Himel Islam <himel7100@gmail.com>
2024-05-11 02:22:33 +0600

    js all cases handeled

--- faq-accordion-main/app.js
+++ faq-accordion-main/app.js
@@ -1,9 +1,21 @@
 (function(){
     const headers = document.querySelectorAll('.toogle-btn');
-    console.log(headers);
+
     headers.forEach(header => {
         header.addEventListener('click', function(){
-            console.log('Clicked', this.parentNode.nextElementSibling);
+            this.parentNode.nextElementSibling.classList.toggle('active');
+
+            const active = this.parentNode.nextElementSibling;
+            const plusImage = this.querySelector('img:first-of-type');
+            const minusImage = this.querySelector('img:last-of-type');
+
+            if(active.classList.contains('active')){
+                plusImage.style.display = 'inline'
+                minusImage.style.display = 'none'
+            }else{
+                plusImage.style.display = 'none'
+                minusImage.style.display = 'inline' 
+            }
         })
     })
     
