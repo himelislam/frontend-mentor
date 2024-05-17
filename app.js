c3b5ee8c7b3afdacd885de6a54f669607433ee92
Himel Islam <himel7100@gmail.com>
2024-05-17 10:50:56 +0600

    advice generator

---
+++ app.js
@@ -0,0 +1,17 @@
+
+<script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/45dce1c893b275b179549d5d849cc0c5.js"></script>
+// <!-- Google Tag Manager -->
+{/* <script> */}
+    
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NFWQ7G6W');
+
+// </script>
+// <!-- End Google Tag Manager -->
+// <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFWQ7G6W"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+// <!-- End Google Tag Manager (noscript) -->
